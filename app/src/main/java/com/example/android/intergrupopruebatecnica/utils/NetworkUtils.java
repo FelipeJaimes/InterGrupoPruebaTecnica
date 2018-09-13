9b9e2b3e30e50c8c18e46cb48ee6f1a368159960
Felipe <felipejg8611@gmail.com>
2018-09-13 11:46:48 -0500

    type: feature.
    Create NetworkUtils class to check internet connection status.

---
+++ app/src/main/java/com/example/android/intergrupopruebatecnica/utils/NetworkUtils.java
@@ -0,0 +1,15 @@
+package com.example.android.intergrupopruebatecnica.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class NetworkUtils {
+
+    public static boolean checkInternetConnection(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED;
+    }
+}
+
